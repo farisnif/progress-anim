ac1e1225431a84dffdb3fe543763efe8c5cea845
farisnif <fan5061@psu.edu>
2023-04-20 14:56:45 -0400

    WHAT HAPPENED

---
+++ src/apps-roster.js
@@ -0,0 +1,68 @@
+import { LitElement, html, css } from 'lit';
+import "./progress-anim.js";
+
+export class AppsRoster extends LitElement {
+    static get tag() {
+        return 'apps-roster';
+    }
+    static get properties() {
+        return {
+            types: { type: Array },
+            apps: { type: String },
+        }
+    }
+
+    constructor() {
+        super();
+        // this.types = [
+        //     {
+        //         "barTitle": "Rspack",
+        //         "numberValue": "50000",
+        //         "introTitle": "Blazing fast build speed",
+        //         "description": "Combining TypeScript and Rust with a parrelized architecture to bring you the ultimate developer experience.",
+        //         "timeLength": "5.79",
+        //         "barWidth": 0.075
+        //     }
+        // ];
+        this.types = []
+        this.apps = 'Software';
+        this.updateRoster();
+    }
+
+    static get styles() {
+        return css`
+        :host {
+
+        }
+        `;
+    }
+
+
+    updateRoster() {
+        const address = new URL('../assets/roster.json', import.meta.url).href;
+        fetch(address).then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            return [];
+        })
+            .then((data) => {
+                this.types = data;
+            });
+    }
+
+
+
+    render() {
+        return html`
+        <div class="wrapper">
+        ${this.types.map(app => html`
+        <div class="item">
+            <progress-anim barTitle="${app.barTitle}" numberValue="${app.numberValue}" introTitle="${app.introTitle}" description="${app.description}" timeLength="${app.timeLength}" barWidth="${app.barWidth}"></progress-anim>
+    </div>
+        `)}
+    </div>
+    `;
+    }
+}
+customElements.define(AppsRoster.tag, AppsRoster);
--- src/progress-anim.js
+++ src/progress-anim.js
@@ -1,20 +1,53 @@
 import { LitElement, html, css } from 'lit';
 
-export class ProgressAnim extends LitElement {
-  static properties = {
-    version: {},
-  };
+import { IntersectionObserverMixin } from "@lrnwebcomponents/intersection-element/lib/IntersectionObserverMixin.js";
+
+export class ProgressAnim extends IntersectionObserverMixin(LitElement) {
+  static get properties() {
+    let props = {};
+    if (super.properties) {
+      props = super.properties;
+    }
+    return {
+      ...props,
+      barTitle: {
+        type: String,
+        reflect: true
+      },
+      numberValue: {
+        type: String,
+        reflect: true
+      },
+      introTitle: {
+        type: String,
+        reflect: true
+      },
+      description: {
+        type: String,
+        reflect: true
+      },
+      timeLength: {
+        type: Number,
+        reflect: true
+      },
+
+      barWidth: {
+        type: Number,
+        reflect: true
+      }
+    }
+  }
 
   static get styles() {
     return css`
-      .progress {
+      #progress {
       background: var(--progress-anim-bar-color, linear-gradient(to right, #9dad6b, #ffa500));
       height: var(--progress-anim-bar-height, 50px);
       border-radius: var(--progress-anim-bar-radius, 6px);
       direction: rtl;
     }
 
-    .timer {
+    #timer {
       padding-right: 10px;
       margin-left: auto;
     }
@@ -52,14 +85,14 @@ export class ProgressAnim extends LitElement {
 
     .surroundingBar {
       margin-top: 50px;
-    padding: 2px;
-    border: 1px solid #c6c5c9;
-    border-radius: 10px;
-    width: 50%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 0 auto;
+      padding: 2px;
+      border: 1px solid #c6c5c9;
+      border-radius: 10px;
+      width: 50%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin: 0 auto;
     }
 
     .moduleCount {
@@ -95,32 +128,84 @@ export class ProgressAnim extends LitElement {
     super();
     this.barTitle = "Rspack";
     this.numberValue = "50000";
+    this.introTitle = "Blazing fast build speed";
+    this.description = "Combining TypeScript and Rust with a parrelized architecture to bring you the ultimate developer experience.";
+    this.timeLength = 5.79;
+    this.barWidth = 0.075;
+  }
+
+  progressAnim(timeLength, barWidth) {
+    var progressBar = this.shadowRoot.querySelector("#progress");
+    var timer = this.shadowRoot.querySelector("#timer");
+    function startTimer(timeLength, barWidth, progressBar, timer) {
+      var totalSeconds = timeLength;
+      var documentWidth =
+        document.documentElement.clientWidth * barWidth;
+      var start = Date.now();
+      var intervalSetted = null;
+
+      function updateTimer() {
+        var diff = (Date.now() - start) / 1000;
+        if (diff >= timeLength) {
+          diff = timeLength;
+          clearInterval(intervalSetted);
+        }
+
+        var seconds = Math.floor(diff % 60);
+        var tenths = Math.floor((diff % 1) * 100);
+        var progressBarWidth = (diff / timeLength) * documentWidth;
+        progressBar.style.width = progressBarWidth + "px";
+        timer.innerHTML =
+          seconds + "." + (tenths < 10 ? "0" + tenths : tenths) + "s";
+      }
+
+      updateTimer();
+      intervalSetted = setInterval(updateTimer, 10);
+    }
+    startTimer(timeLength, barWidth, progressBar, timer);
+  }
+
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      if (
+        ["elementVisible"].includes(
+          propName
+        )
+      ) {
+        console.log(this.elementVisible);
+        clearTimeout(this._debounce);
+        this._debounce = setTimeout(() => {
+          this.progressAnim(this.timeLength, this.barWidth);
+        }, 10);
+      }
+    });
   }
 
   render() {
     return html`
     
-      <div class="bruh1">Curry for 3</div>
-      <div class="bruh2">LeBron for 3</div>
+    <div class="bruh1">Curry for 3</div>
+    <div class="bruh2">LeBron for 3</div>
 
-      <div class="subfiller">
-        <div class="speedText">Blazing fast build speed</div>
-        <div class="textDescription">Combining TypeScript and Rust with a parrelized architecture to bring you the ultimate developer experience.</div>
-      </div>
+    <div class="subfiller">
+      <div class="speedText">${this.introTitle}</div>
+      <div class="textDescription">${this.description}</div>
+    </div>
 
-      <div class="progressArea">
-        <div class="barTitle">${this.barTitle}</div>
-      <div class="surroundingBar">
-        <div class="progress"></div>
-        <div class="timer"></div>
-      </div>
-  </div>
+    <div class="progressArea">
+      <div class="barTitle">${this.barTitle}</div>
+    <div class="surroundingBar">
+      <div id="progress"></div>
+      <div id="timer"></div>
+    </div>
+</div>
 
-      <div class="moduleCount">
-        <div class="moduleCountDefn">Module Count: </div>
-        <div class="moduleCountNumber">${this.numberValue}</div>
-      </div>
-    `;
+    <div class="moduleCount">
+      <div class="moduleCountDefn">Module Count: </div>
+      <div class="moduleCountNumber">${this.numberValue}</div>
+    </div>
+  `;
   }
 }
 
