8d0ee5a2a49072fa07e8af6972de67f563555a48
farisnif <fan5061@psu.edu>
2023-05-02 18:01:05 -0400

    yo if u don't

--- src/progress-anim.js
+++ src/progress-anim.js
@@ -271,7 +271,7 @@ export class ProgressAnim extends IntersectionObserverMixin(LitElement) {
       var prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 
       // update the progress bar every 100 milliseconds for a smoother animation
-      intervalSetted = setInterval(updateTimer, prefersReducedMotion ? 2000 : 400);
+      intervalSetted = setInterval(updateTimer, prefersReducedMotion ? 2000 : 300);
     }
 
     startTimer(timeLength, barWidth, progressBar, timer, maxTime);
